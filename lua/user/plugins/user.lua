ebd3a13b0f0ebcef91a75e5d699263aa52fbb445
Roman-Ojha <razzroman98@gmail.com>
2023-04-19 23:48:40 +0545

    feat: added todo-comments & typescript plugins inside user

--- lua/user/plugins/user.lua
+++ lua/user/plugins/user.lua
@@ -1,3 +1,5 @@
+-- TODO: Test
+
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
@@ -9,4 +11,18 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    -- config = function()
+    --  require("todo-comments").setup {
+    --  }
+    -- end
+    opts = {},
+    event = "User AstroFile",
+    cmd = { "TodoQuickFix" },
+    keys = {
+      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open Todos in Telescope" },
+    },
+  },
 }
