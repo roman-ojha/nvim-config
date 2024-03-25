ecbb910554a939f721842263a473d5c52161d24f
Roman-Ojha <80277272+roman-ojha@users.noreply.github.com>
2024-03-25 21:49:58 +0545

    refactor: small wilder changes

--- plugins/config/wilder.lua
+++ plugins/config/wilder.lua
@@ -33,18 +33,19 @@ wilder.set_option("pipeline", {
   ),
 })
 
-local blue_accent = wilder.make_hl("WilderAccent", "Pmenu", { { a = 1 }, { a = 1 }, { foreground = "#f50f3d" } })
+local blue_accent = wilder.make_hl("WilderAccent", "Pmenu", { { a = 1 }, { a = 1 }, { foreground = "#FF204E" } })
 
 local popupmenu_renderer = wilder.popupmenu_renderer(
+  -- 'popupmenu_palette_theme' to position it at the center of screen
   wilder.popupmenu_palette_theme {
     -- 'single', 'double', 'rounded' or 'solid'
     -- can also be a list of 8 characters, see :h wilder#popupmenu_palette_theme() for more details
     border = "rounded",
-    max_height = "65%",      -- max height of the palette
-    min_height = 0,          -- set to the same as 'max_height' for a fixed height window
+    max_height = "45%", -- max height of the palette
+    min_height = 0, -- set to the same as 'max_height' for a fixed height window
     max_width = "40%",
     prompt_position = "top", -- 'top' or 'bottom' to set the location of the prompt
-    reverse = 0,             -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
+    reverse = 0, -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
     empty_message = wilder.popupmenu_empty_message_with_spinner(),
     highlighter = wilder.basic_highlighter(),
     highlights = {
@@ -63,28 +64,59 @@ local popupmenu_renderer = wilder.popupmenu_renderer(
       wilder.popupmenu_scrollbar(),
     },
   }
--- wilder.popupmenu_border_theme({
---     border = 'rounded',
---     empty_message = wilder.popupmenu_empty_message_with_spinner(),
---     highlighter = wilder.basic_highlighter(),
---     highlights = {
---         accent = blue_accent,
---     },
---     left = {
---         ' ',
---         wilder.popupmenu_devicons(),
---         wilder.popupmenu_buffer_flags({
---             flags = ' a + ',
---             icons = { ['+'] = '', a = '', h = '' },
---         }),
---     },
---     right = {
---         ' ',
---         wilder.popupmenu_scrollbar(),
---     },
--- })
+
+  -- position it at the bottom of the screen
+  -- wilder.popupmenu_border_theme {
+  --   border = "rounded",
+  --   max_height = "45%", -- max height of the palette
+  --   min_height = 0, -- set to the same as 'max_height' for a fixed height window
+  --   max_width = "40%",
+  --   -- reverse = 0, -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
+  -- empty_message = wilder.popupmenu_empty_message_with_spinner(),
+  --   highlighter = wilder.basic_highlighter(),
+  --   highlights = {
+  --     accent = blue_accent,
+  --   },
+  --   left = {
+  --     " ",
+  --     wilder.popupmenu_devicons(),
+  --     wilder.popupmenu_buffer_flags {
+  --       flags = " a + ",
+  --       icons = { ["+"] = "", a = "", h = "" },
+  --     },
+  --   },
+  --   right = {
+  --     " ",
+  --     wilder.popupmenu_scrollbar(),
+  --   },
+  -- }
 )
 
+-- local popupmenu_renderer = wilder.popupmenu_renderer(wilder.popupmenu_border_theme {
+--   border = "rounded",
+--   max_height = "45%", -- max height of the palette
+--   min_height = 0, -- set to the same as 'max_height' for a fixed height window
+--   max_width = "40%",
+--   reverse = 0, -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
+--   empty_message = wilder.popupmenu_empty_message_with_spinner(),
+--   highlighter = wilder.basic_highlighter(),
+--   highlights = {
+--     accent = blue_accent,
+--   },
+--   left = {
+--     " ",
+--     wilder.popupmenu_devicons(),
+--     wilder.popupmenu_buffer_flags {
+--       flags = " a + ",
+--       icons = { ["+"] = "", a = "", h = "" },
+--     },
+--   },
+--   right = {
+--     " ",
+--     wilder.popupmenu_scrollbar(),
+--   },
+-- })
+--
 local wildmenu_renderer = wilder.wildmenu_renderer {
   highlighter = wilder.basic_highlighter(),
   highlights = {
