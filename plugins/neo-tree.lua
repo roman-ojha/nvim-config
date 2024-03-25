6f937e182df834067f22d6236a4f6998300f4c0d
Roman-Ojha <80277272+roman-ojha@users.noreply.github.com>
2024-03-25 12:49:00 +0545

    feat: neo-tree config to open img, pdf prevew on default os application

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -1,28 +1,5 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    default_component_configs = {
-      indent = {
-        padding = 1,
-      },
-    },
-    source_selector = {
-      winbar = false,
-      statusline = false,
-    },
-    filesystem = {
-      -- list of directories to hide in the tree
-      filtered_items = {
-        visible = false,
-        hide_dotfiles = false,
-        hide_gitignored = false,
-        hide_by_name = {
-          -- ".DS_Store",
-          -- "thumbs.db",
-          -- "node_modules",
-          -- "__pycache__",
-        },
-      },
-    },
-  },
+  -- dependencies = { "MunifTanjim/nui.nvim", "3rd/image.nvim" },
+  opts = function(_, config) return require "user.plugins.config.neo-tree" end,
 }
